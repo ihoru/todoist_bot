9e3174cddf62ff0abfe371a58e204803b0ab9728
ihoru <i.h.o.r.p.o.l.y.a.k.o.v@gmail.com>
2019-12-30 23:09:36 +0700

    Use python3.6 instead of 3.5

--- scripts/virtualenv.sh
+++ scripts/virtualenv.sh
@@ -5,5 +5,5 @@
 
 DIR=`dirname $0`;
 cd ${DIR}/..;
-virtualenv -p python3.5 --system-site-packages ENV;
+virtualenv -p python3.6 --system-site-packages ENV;
 # --system-site-packages is needed if you want to be able to use jupyter and ipython if they are installed system-wide
